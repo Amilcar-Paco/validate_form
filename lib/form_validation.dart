26c9b84036009bd17fef91cd6a588ea9933be610
Amílcar Paco <amilcarcpaco@gmail.com>
2020-08-19 16:30:56 +0200

    Initial commit

---
+++ lib/form_validation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormValidation extends StatefulWidget {
+  @override
+  _FormValidationState createState() => _FormValidationState();
+}
+
+class _FormValidationState extends State<FormValidation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
